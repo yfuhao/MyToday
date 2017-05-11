9f76a25205936c7b8b8a02d35d3166bbf2e80864
jjp0136 <qwqa5210>
2017-05-11 14:58:05 +0800

    UI跟新界面2.0.1

--- app/src/main/java/text/bwie/today/RegisterPhoneActivity.java
+++ app/src/main/java/text/bwie/today/RegisterPhoneActivity.java
@@ -1,33 +1,33 @@
 package text.bwie.today;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
- * Created by lenovo-pc on 2017/5/10.
+ * Created by lenovo-pc on 2017/5/11.
  */
 
-public class RegisterActivity extends Activity {
+public class RegisterPhoneActivity extends Activity {
 
-    private Button btn_phone;
+    private EditText user_phone;
+    private EditText user_password;
+    private TextView tv_register;
+    private Button btn_register;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mroeregister);
-
+        setContentView(R.layout.activity_login);
         init();
-
     }
 
     private void init() {
-        btn_phone = (Button) findViewById(R.id.register_phone);
         ImageView image_back= (ImageView) findViewById(R.id.left_head_back);
         image_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,13 +35,11 @@ public class RegisterActivity extends Activity {
                 finish();
             }
         });
-        btn_phone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(RegisterActivity.this,RegisterPhoneActivity.class);
-                startActivity(intent);
-            }
-        });
+        user_phone = (EditText) findViewById(R.id.register_activity_edit_phone);
+        user_password = (EditText) findViewById(R.id.register_activity_edit_password);
+        tv_register = (TextView) findViewById(R.id.register_activity_text_register);
+        btn_register = (Button) findViewById(R.id.register_activity_btn_register);
+
 
 
     }
